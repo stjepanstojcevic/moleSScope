f2b0e06a5d940d5ff84149ee87cf5dbe9eb17de2
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-10-30 11:20:00 +0100

    Initial Commit

---
+++ moleSScope/moleSScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  moleSScopeApp.swift
+//  moleSScope
+//
+//  Created by Stjepan Stojčević on 30.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct moleSScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
